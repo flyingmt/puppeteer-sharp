9a507b02a73e39fb9a41f9f6b9df3026d860898e
Darío Kondratiuk <dariokondratiuk@gmail.com>
2018-04-17 08:33:01 -0300

    Restore tests

--- lib/PuppeteerSharp.Tests/Puppeteer/PuppeteerConnectTests.cs
+++ lib/PuppeteerSharp.Tests/Puppeteer/PuppeteerConnectTests.cs
@@ -10,48 +10,65 @@ namespace PuppeteerSharp.Tests.Puppeteer
         [Fact]
         public async Task ShouldBeAbleToConnectMultipleTimesToSameBrowser()
         {
-            var options = new ConnectOptions()
+            var originalOptions = TestConstants.DefaultBrowserOptions();
+            var launcher = new Launcher();
+
+            using (var originalBrowser = await launcher.LaunchAsync(originalOptions, TestConstants.ChromiumRevision))
             {
-                BrowserWSEndpoint = Browser.WebSocketEndpoint
-            };
-            var browser = await PuppeteerSharp.Puppeteer.ConnectAsync(options);
-            using (var page = await browser.NewPageAsync())
-            {
-                var response = await page.EvaluateExpressionAsync<int>("7 * 8");
-                Assert.Equal(response, 56);
+                var options = new ConnectOptions()
+                {
+                    BrowserWSEndpoint = originalBrowser.WebSocketEndpoint
+                };
+                var browser = await PuppeteerSharp.Puppeteer.ConnectAsync(options);
+                using (var page = await browser.NewPageAsync())
+                {
+                    var response = await page.EvaluateExpressionAsync<int>("7 * 8");
+                    Assert.Equal(response, 56);
+                }
+
+                using (var originalPage = await originalBrowser.NewPageAsync())
+                {
+                    var response = await originalPage.EvaluateExpressionAsync<int>("7 * 6");
+                    Assert.Equal(response, 42);
+                }
             }
 
-            using (var originalPage = await Browser.NewPageAsync())
-            {
-                var response = await originalPage.EvaluateExpressionAsync<int>("7 * 6");
-                Assert.Equal(response, 42);
-            }
+            Assert.True(launcher.IsChromeClosed);
         }
 
         [Fact]
         public async Task ShouldBeAbleToReconnectToADisconnectedBrowser()
         {
+            var originalOptions = TestConstants.DefaultBrowserOptions();
+            var launcher = new Launcher();
+
+            var originalBrowser = await launcher.LaunchAsync(originalOptions, TestConstants.ChromiumRevision);
             var options = new ConnectOptions()
             {
-                BrowserWSEndpoint = Browser.WebSocketEndpoint
+                BrowserWSEndpoint = originalBrowser.WebSocketEndpoint
             };
 
-            var url = TestConstants.CrossProcessHttpPrefix + "/frames/frame.html";
-            var page = await Browser.NewPageAsync();
-            await page.GoToAsync(url);
+            var page = await originalBrowser.NewPageAsync();
+            await page.GoToAsync(TestConstants.CrossProcessHttpPrefix + "/frames/nested-frames.html");
 
-            Browser.Disconnect();
+            originalBrowser.Disconnect();
 
             using (var browser = await PuppeteerSharp.Puppeteer.ConnectAsync(options))
             {
                 var pages = (await browser.Pages()).ToList();
-                var restoredPage = pages.FirstOrDefault(x => x.Url == url);
+                var restoredPage = pages.FirstOrDefault(x => x.Url == TestConstants.CrossProcessHttpPrefix + "/frames/nested-frames.html");
                 Assert.NotNull(restoredPage);
                 var frameDump = FrameUtils.DumpFrames(restoredPage.MainFrame);
-                Assert.Equal(@"http://127.0.0.1:<PORT>/frames/frame.html", frameDump);
+                Assert.Equal(@"http://127.0.0.1:<PORT>/frames/nested-frames.html
+    http://127.0.0.1:<PORT>/frames/two-frames.html
+        http://127.0.0.1:<PORT>/frames/frame.html
+        http://127.0.0.1:<PORT>/frames/frame.html
+    http://127.0.0.1:<PORT>/frames/frame.html", frameDump);
                 var response = await restoredPage.EvaluateExpressionAsync<int>("7 * 8");
                 Assert.Equal(response, 56);
             }
+
+            Assert.True(launcher.IsChromeClosed);
         }
     }
 }
